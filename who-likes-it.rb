a6d4fd0aec5f36d7143a93822bf3cd74b085acc6
Mike Moore <mikemoore1618@gmail.com>
2019-11-14 14:59:04 +0700

    add psuedo code

--- who-likes-it.rb
+++ who-likes-it.rb
@@ -8,3 +8,19 @@
 # likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
 # likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others
 
+# --------------------------------------------------------------------
+
+# if likes = nil, likes = [], then return "no one likes this"
+# if likes.length === 1, likes = ["Mike"], then return "Mike likes this"
+
+# if likes.length == 2, ["Mike, "Dusty"] then, return " Mike and Dusty like this"
+
+# if likes.length  == 3, ["Mike, "Dusty", "Talon"] then, return " Mike, Dusty, and Talon like this"
+
+# if likes.length > 3, return "likes[0], likes[1], and likes.length - 2 others like this"
+# ["Mike, "Dusty", "Talon", "Ryan"] then, return " Mike, Dusty, and 2 others like this"
+
+def likes(names)
+    #your code here
+end
+
