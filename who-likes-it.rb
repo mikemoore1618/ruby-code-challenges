899712820fbd69de7abb7a9295e02e4f68401db3
Mike Moore <mikemoore1618@gmail.com>
2019-11-14 15:11:11 +0700

    final

--- who-likes-it.rb
+++ who-likes-it.rb
@@ -21,6 +21,11 @@
 # ["Mike, "Dusty", "Talon", "Ryan"] then, return " Mike, Dusty, and 2 others like this"
 
 def likes(names)
-    #your code here
-end
+    return "no one likes this" if names.empty? == true
+    return "#{names[0]} likes this" if names.count == 1 
+    return "#{names[0]} and #{names[1]} like this" if names.count == 2 
+    return "#{names[0]}, #{names[1]} and #{names[2]} like this" if names.count == 3
+    return "#{names[0]}, #{names[1]} and 2 others like this"  if names.count == 4
+    return "#{names[0]}, #{names[1]} and #{names.count - 2} others like this"  if names.count > 4
+end 
 
