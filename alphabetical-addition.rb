d47cfee65245f389a5cfe97f498b13968d2bfcb6
Mike Moore <mikemoore1618@gmail.com>
2019-11-17 17:11:30 +0700

    remove smallest, add instruction notes

--- alphabetical-addition.rb
+++ alphabetical-addition.rb
@@ -16,3 +16,18 @@
 
 #------------------------------------------------------------------------------------------------------
 
+# create array for entire alphabet
+# assign numeric vales to alphabet array using index # + 1
+# if 
+
+def add_letters(*letters)
+    return 'z' if letters.empty?
+    alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+    # %w(foo bar) is a shortcut for ["foo", "bar"]
+    index = 0
+    letters.each do |letter|
+      index += (alphabet.index(letter) + 1)
+    end
+    puts index
+    alphabet[(index - 1) % 26]
+  end
